55ae1f87fc6119c62a2fbfb3223813a203022622
KeMa <keskimattinen.konsta@gmail.com>
2023-07-31 18:18:00 +0300

    removed debug logging

--- server/auth/validateToken.js
+++ server/auth/validateToken.js
@@ -2,9 +2,7 @@ const jwt = require("jsonwebtoken");
 
 module.exports = function validateToken(req, res, next) {
     console.log("Validating token");
-    console.log(req.headers)
     const authHeader = req.headers["authentication"];
-    console.log(authHeader);
     let token;
     if (authHeader) {
         token = authHeader.split(" ")[1];
@@ -12,6 +10,7 @@ module.exports = function validateToken(req, res, next) {
         token = null;
     }
     if (token == null) return res.sendStatus(401);
+
     console.log("Token found");
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
         if (err) return res.sendStatus(403);
