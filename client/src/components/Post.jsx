cfa27fd200a6af71088aff36419f0582ce98db34
KeMa <keskimattinen.konsta@gmail.com>
2023-07-31 18:20:03 +0300

    Post to work with new api

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,28 +1,74 @@
-import { Card, CardContent, CardHeader } from "@mui/material";
+import { Card, CardActionArea, CardContent, CardHeader, FormControl, Typography, TextField, Button } from "@mui/material";
 import Comment from "./Comment";
 import {useState, useEffect} from 'react';
+import { useIsAuthenticated, useAuthHeader } from "react-auth-kit";
 
-export default function Post(props) {
+export default function Post({id}) {
     const [post, setPostData] = useState({})
+    const [comments, setComments] = useState([])
+    const isAuthenticated = useIsAuthenticated();
+    const token = useAuthHeader();
 
     useEffect(() => {
         let isMounted = true;
-
-        fetch(`/api/post/${props.id}`).then(response => response.json()).then(json => {
+        fetch(`/api/post/${id}`).then(response => response.json()).then(json => {
+            console.log(json);
             if(isMounted) setPostData(json);
         }).catch(console.error);
 
+        fetch(`/api/post/${id}/comment`).then(response => response.json()).then(json => {
+            console.log(json);
+            if(isMounted) setComments(json);
+        }).catch(console.error);
+
         return () => { isMounted = false };
-    }, [props.id])
+    }, [id])
+
+    const submitComment = (e) => {
+        e.preventDefault();
+        if(!isAuthenticated()){
+            alert("You need to be logged in to comment!");
+            return;
+        } 
+        const data = {
+            "comment": e.target.comment.value,
+            "postId": id
+        }
+        fetch(`/api/post/comment`, {
+            method: "POST",
+            headers: {
+                "Authentication": token(),
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(data),
+            mode: "cors",
+        }).then(response => response.json()).then(json => {
+            console.log(json);
+            setComments([...comments, json]);
+        }).catch(console.error);
+    }
 
 
     return <Card variant="outlined">
-        <CardHeader title={post?.title} />
+        <CardHeader title={post?.postTitle} />
+        <h1>Title</h1>
+        <Typography>
+            {post?.posterName}
+        </Typography>
         <CardContent>
-            {post?.content}
+            {post?.postContent}
         </CardContent>
-        <ul>
-           {post?.comments.map((comment) => <li key={comment.id}><Comment comment={comment}/></li>)}
-        </ul>
+        <CardActionArea>
+            <Typography>Comments</Typography>
+            <form onSubmit={submitComment}>
+                <FormControl>
+                    <TextField equired={true} name="comment" id="text" label="Write your comment!" />
+                    <Button variant="primary" type="submit">POST!</Button>
+                </FormControl>
+            </form>
+            <ul>
+                {comments.map((comment) => <li key={comment.id}><Comment comment={comment.comment} username={comment.username}/></li>)}
+            </ul>
+        </CardActionArea>
     </Card>
 }
